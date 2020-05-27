1e2822b5058ecc736d0c0629aebfd8938883e330
John Achero <jacher@student.fullsail.edu>
2020-05-27 03:37:17 -0400

    Fetch

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+// import React, { Component } from "react";
 
-class Profile extends Component {
-  render() {
-    return <section>Profile</section>;
-  }
-}
+const Profile = () => {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  //create state to add items in
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch("https://randomuser.me/api/?gender=female");
+
+    //convert to json
+    const items = await data.json();
+    console.log(items.results);
+    setItems(items.results);
+  };
+  return (
+    <div className="App">
+      {items.map((item) => (
+        <h1 key={item.resultsId}>{item.phone}</h1>
+      ))}
+    </div>
+  );
+};
 
 export default Profile;
